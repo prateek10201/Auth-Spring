680f111829131a12897f3e89db3ceaff4acc5682
Prateek <prateek.10201@gmail.com>
2021-11-13 16:58:39 +0530

    cors configure

--- src/main/java/com/sfuit/Auth/contorller/DeviceController.java
+++ src/main/java/com/sfuit/Auth/contorller/DeviceController.java
@@ -5,15 +5,13 @@ import com.sfuit.Auth.services.DeviceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/users")
 public class DeviceController {
 
